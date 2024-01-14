5f1d68d4c39812c97975e8296dcd7b61208ed8df
lephanbaongoc <lpbn2003@gmail.com>
2024-01-14 12:23:36 +0700

    Ví dụ đầu tiên
    Chỉ để Demo

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tôi lần đầu học Java và đang thử nghiệm quản lý mã nguồn với GIT");
+
+	}
+
+}
