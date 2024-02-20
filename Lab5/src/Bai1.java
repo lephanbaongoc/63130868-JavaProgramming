e4ff6e5eaf2d418288c574fb65a0d8beab2f8c92
lephanbaongoc <lpbn2003@gmail.com>
2024-02-20 16:17:18 +0700

    Lab5 Bai1 done

---
+++ Lab5/src/Bai1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Double S = 0.0;
+		Scanner s = new Scanner(System.in);
+		ArrayList<Double> ls = new ArrayList<Double>();
+		
+		System.out.print("Nhập ds số thực từ bàn phím: \n");
+		
+		while(true) {
+			System.out.print("\nNhập số thực: ");
+			Double x = s.nextDouble();
+			ls.add(x);
+			
+			System.out.print("Nhập thêm (Y/N)? ");
+			if(s.next().equals("N")) {
+				break;
+			}
+		}
+		
+		for(Double x : ls){
+			System.out.print("\nDANH SÁCH CÁC SỐ THỰC:\n");
+			System.out.println(x);
+			S += x;
+		}
+		System.out.print("Tổng các số thực là: " + S);
+	}
+
+}
